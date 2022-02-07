8e3622ec441fb6d0356dbbd957bcd1fdafe9a0e0
GUNTHA ROHITH <48819698+guntharohith@users.noreply.github.com>
2022-02-07 09:39:48 +0530

    Update index.js

--- backend/email/index.js
+++ backend/email/index.js
@@ -2,8 +2,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'shettyrohith968@gmail.com',
-        pass: 'igiujzaohxtjmwis'
+        user: '',
+        pass: ''
     }
 });
 
